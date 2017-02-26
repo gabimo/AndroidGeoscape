5a1ecba037cc812f9ed65d691c431c8f1b6802e3
Michael <mellis40@uncc.edu>
2017-02-26 12:31:45 -0500

    Can delete a request to do a job

--- app/src/main/java/com/lawnscape/ViewJobRequestsActivity.java
+++ app/src/main/java/com/lawnscape/ViewJobRequestsActivity.java
@@ -1,8 +1,13 @@
 package com.lawnscape;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -54,5 +59,69 @@ public class ViewJobRequestsActivity extends Activity {
         jobRequestersRef = database.getReference("Users");
         jobRequestersRef.addListenerForSingleValueEvent(
                 new UserListVEListener(ViewJobRequestsActivity.this, userList, usersToDisplay, reuesterAdapter));
+        // need to add some onclick listeners
+        //This handles clicks on individual user items from the list
+        // and bring you to a job specific chat page with the user
+        allRequestersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                User selectedUser = (User) reuesterAdapter.getItem(position);
+                Intent chatIntent = new Intent(ViewJobRequestsActivity.this, ChatActivity.class);
+                chatIntent.putExtra("otherid", selectedUser.getUserid());
+                startActivity(chatIntent);
+            }
+        });
+        // Hold down on a user in the chat list to get a popup
+        allRequestersList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position,
+                                           long id) {
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(ViewJobRequestsActivity.this, view);
+                popup.getMenuInflater().inflate(R.menu.popup_assign_jobs, popup.getMenu());
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+
+                        final User selectedUser = (User) reuesterAdapter.getItem(position);
+                        switch (item.getItemId()){
+                            case R.id.longclickAssignJob:
+                                
+                                return true;
+                            case R.id.longclickDeleteChat:
+                                final FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                //remove the job from the list of all jobs with a listener
+                                DatabaseReference jobRef = database.getReference("Jobs").child(selectedJob.getPostid()).child("requesters");
+
+                                jobRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        for(DataSnapshot reqUser: dataSnapshot.getChildren()) {
+                                            System.out.println(reqUser.getKey());
+                                            if (reqUser.getValue().toString().equals(selectedUser.getUserid())) {
+                                                reqUser.getRef().removeValue();
+                                            }
+                                        }
+                                    }
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {  }
+                                });
+                                userList.remove(selectedUser);
+                                reuesterAdapter.notifyDataSetChanged();
+                                return true;
+                            case R.id.longclickViewProfile:
+                                Intent viewProfileIntent = new Intent(ViewJobRequestsActivity.this, ViewUserProfileActivity.class);
+                                viewProfileIntent.putExtra("UserID", selectedUser.getUserid());
+                                startActivity(viewProfileIntent);
+                                return true;
+                        }
+                        return true;
+                    }
+                });
+                popup.show();//showing popup menu
+                return true;
+            }
+        });
     }
 }
